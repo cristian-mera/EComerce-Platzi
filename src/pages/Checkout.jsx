3f70f09aed290b045aaf69a8ffb0d8f1fdd3ea33
Cristian Mera <cristianlmera@gmail.com>
2022-02-04 19:52:01 -0500

    Se crean las pages, containers, components, y sus respectivas rutas, todo esta funcional

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
+import OrderItem from '../components/OrderItem';
+import Menu from '../components/Menu';
+import '../styles/Checkout.scss';
 
 const Checkout = () => {
-    return (
-        <div class="Checkout">
-            <div class="Checkout-container">
-                <h1 class="title">My order</h1>
-
-                <div class="Checkout-content">
-                    <div class="order">
-                        <p>
-                            <span>03.25.21</span>
-                            <span>6 articles</span>
-                        </p>
-                        <p>$560.00</p>
-                    </div>
-                </div>
-                <OrderItem />
-            </div>
-        </div>
-    );
-};
+	return (
+		<div className="Checkout">
+			<div className="Checkout-container">
+				<h1 className="title">My order</h1>
+				<div className="Checkout-content">
+					<div className="order">
+						<p>
+							<span>03.25.21</span>
+							<span>6 articles</span>
+						</p>
+						<p>$560.00</p>
+					</div>
+				</div>
+				<OrderItem />
+			</div>
+		</div>
+	);
+}
 
 export default Checkout;
